b066a4ae03fe6ca50ddf03b4c7d0f75ea697abd2
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-11-01 16:29:09 +0530

    feat: configured toasts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,15 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { Toaster } from "react-hot-toast";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Toaster containerClassName="font-mono" />
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
