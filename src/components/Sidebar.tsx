15a400946c39e78ce7194ac688b204c15c49fc7f
pybash1 <67195650+pybash1@users.noreply.github.com>
2023-11-01 16:27:55 +0530

    feat(components): added all components

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,81 @@
+import { MONTHS, DAYS } from "~/utils/constants";
+import Task from "./Task";
+import { api } from "~/utils/api";
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+const Sidebar = () => {
+  // const [date] = useState(new Date());
+  const [task, setTask] = useState("");
+  const { mutateAsync: createTask } = api.tasks.create.useMutation();
+  const { mutateAsync: initTask } = api.tasks.init.useMutation();
+  const { data: tasks, isLoading } = api.tasks.getAll.useQuery();
+
+  const getDone = (task: {
+    key: string;
+    habit: string;
+    days: Record<string, boolean>;
+  }) => {
+    const day = new Date(new Date().setHours(0, 0, 0, 0));
+    const status =
+      task.days[`${day.getDate()}-${day.getMonth()}-${day.getFullYear()}`];
+
+    if (typeof status !== "boolean")
+      void initTask({ id: task.key, date: new Date() });
+
+    return status ?? false;
+  };
+
+  return (
+    <aside className="sticky top-0 flex h-screen flex-col gap-10 bg-gray-700 py-8">
+      <div className="flex items-center justify-between px-12 uppercase">
+        <div className="flex items-center gap-1">
+          <div className="text-5xl">{new Date().getDate()}</div>
+          <div className="flex flex-col leading-5">
+            <div>{MONTHS[new Date().getMonth()]}</div>
+            <div>{new Date().getFullYear()}</div>
+          </div>
+        </div>
+        <div>{DAYS[new Date().getDay()]}</div>
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          void toast.promise(createTask({ task, done: false }), {
+            success: "Created habit!",
+            error: "Failed to create habit!",
+            loading: "Creating...",
+          });
+          setTask("");
+        }}
+        className="flex w-full flex-col gap-2 px-12"
+      >
+        <input
+          type="text"
+          className="w-full px-3 py-2 text-black outline-none"
+          placeholder="Start XYZ habit from today"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button className="w-full bg-black py-2 hover:bg-white hover:text-black">
+          Start new Habit
+        </button>
+      </form>
+      <div className="flex flex-col gap-2 overflow-y-scroll px-12">
+        {tasks
+          ?.sort((t) => (!getDone(t) ? -1 : 1))
+          .map((task) => (
+            <Task
+              key={task.key}
+              id={task.key}
+              task={task.habit}
+              done={getDone(task)!}
+            />
+          ))}
+        {isLoading ? <div>Loading tasks for today...</div> : null}
+      </div>
+    </aside>
+  );
+};
+
+export default Sidebar;
---
+++ src/components/Task.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useRef, useState } from "react";
+import { api } from "~/utils/api";
+
+const Task = ({ id, task, done }: Props) => {
+  const [done_, setDone] = useState(done);
+  const [ctxMenu, setCtxMenu] = useState(false);
+  const { mutateAsync: markAsDone } = api.tasks.markAsDone.useMutation();
+  const { mutateAsync: archive } = api.tasks.archive.useMutation();
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  const ctx = api.useContext();
+
+  useEffect(() => {
+    const handleClose = (e: MouseEvent) => {
+      if (ref.current && !ref.current.contains(e.target as Node))
+        setCtxMenu(false);
+    };
+
+    document.addEventListener("click", handleClose);
+
+    return () => {
+      document.removeEventListener("click", handleClose);
+    };
+  }, []);
+
+  return (
+    <div
+      className="flex items-center gap-3"
+      onContextMenu={(e) => {
+        e.preventDefault();
+        setCtxMenu(true);
+      }}
+      ref={ref}
+    >
+      <button
+        onClick={() => {
+          setDone(!done_);
+          void markAsDone({ id, date: new Date(), done });
+          void ctx.tasks.invalidate();
+        }}
+        className={`h-4 w-4 outline-none ring-2 ring-white focus:ring-black ${
+          done_ ? "bg-white" : null
+        }`}
+      ></button>
+      {done_ ? <s>{task}</s> : <div>{task}</div>}
+      {ctxMenu ? (
+        <div className="absolute bg-white px-4 py-2 text-black">
+          <button onClick={() => void archive({ id })}>Archive</button>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+interface Props {
+  id: string;
+  task: string;
+  done: boolean;
+}
+
+export default Task;
